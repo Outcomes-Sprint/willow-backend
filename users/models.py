46fe48554a396dc15f93e3e2da0900c625aa4c8d
atlfinesse <finesse.cap@gmail.com>
2021-03-04 11:46:48 -0800

    Updated user models

--- users/models.py
+++ users/models.py
@@ -1,9 +1,42 @@
-from django.contrib.auth.models import AbstractUser
 from django.db import models
-# Create your models here.
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 
-class User(AbstractUser):
-    name = models.CharField(max_length=250, blank=False)
-    email = models.EmailField(verbose_name='email', max_length=255, unique=True)
-    approved = models.BooleanField(default=False)
 
+class UserManager(BaseUserManager):
+
+    def create_user(self, email, name, password=None, **extra_fields):
+        if not email:
+            raise ValueError('User must have an email address')
+
+        user = self.model(email=self.normalize_email(email), name=name)
+
+        user.set_password(password)
+        user.save()
+
+        return user
+
+    def create_superuser(self, email, name, password):
+
+        user = self.create_user(email, name, password)
+
+        user.is_superuser = True
+        user.is_staff = True
+        user.save()
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    def __str__(self):
+        return self.email
