da5657450050ecbfb07d8baeb5054909d5c08369
atlfinesse <finesse.cap@gmail.com>
2021-03-04 15:41:03 -0800

    Merged donation and donor into donor model

--- donations/models.py
+++ donations/models.py
@@ -3,12 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Donation(models.Model):
-    date = models.DateField(auto_now=True)
-    # amount = models.PositiveIntegerField()
-    # userID -> ObjectID
-
-
-class Donor(models.Model):
     name = models.TextField(max_length=255, blank=False)
     email = models.EmailField(max_length=254, unique=True)
-    # donation = models.ForeignKey(Donation, on_delete=models.CASCADE, related_name='donors', blank=False)
+    amount = models.PositiveIntegerField(default=0)
+    date = models.DateField(auto_now=True)
--- donations/serializers.py
+++ donations/serializers.py
@@ -4,9 +4,4 @@ from .models import Donation, Donor
 class DonationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Donation
-        fields = ('date')
-
-class DonorSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Donor
-        fields = ('name', 'email')
+        fields = ('name', 'email', 'amount','date')
