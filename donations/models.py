e13c28fc20ad61932393431f9384cedef45c1a32
atlfinesse <finesse.cap@gmail.com>
2021-03-05 10:21:05 -0800

    Added workind date field for donations models
    The next step is to connect the properties to everything

--- donations/models.py
+++ donations/models.py
@@ -4,9 +4,9 @@ from django.db import models
 
 class Donation(models.Model):
     name = models.CharField(max_length=40, blank=False)
-    email = models.EmailField(max_length=255, unique=True)
+    email = models.EmailField(max_length=255)
     amount = models.PositiveIntegerField(blank=False, null=True)
     date = models.DateField(auto_now=True)
 
     def __str__(self):
-        return self.name
+        return self.amount
--- donations/serializers.py
+++ donations/serializers.py
@@ -3,11 +3,6 @@ from .models import Donation
 
 
 class DonationSerializer(serializers.ModelSerializer):
-    date = serializers.ReadOnlyField(
-        source='donation.date',
-        read_only=True
-    )
-
     class Meta:
         model = Donation
         fields = ('name', 'email', 'amount', 'date')
