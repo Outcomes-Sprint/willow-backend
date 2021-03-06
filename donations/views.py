3da5ed781b2ca04c5d51d6ae6401e5492cfb3516
atlfinesse <finesse.cap@gmail.com>
2021-03-06 12:38:40 -0800

    Successful donation post as superuser

--- donations/views.py
+++ donations/views.py
@@ -6,12 +6,27 @@ from .serializers import DonationSerializer
 from .permissions import IsOwner
 
 
+# class DonationViewSet(viewsets.ModelViewSet):
+#     serializer_class = DonationSerializer
+#     queryset = Donation.objects.all()
+
+#     def get_queryset(self, *args, **kwargs):
+#         return Donation.objects.all()
+
+#     def perform_create(self, serializer):
+#         serializer.save()
+
+
 class DonationViewSet(viewsets.ModelViewSet):
     serializer_class = DonationSerializer
-    queryset = Donation.objects.all()
-
-    def get_queryset(self, *args, **kwargs):
-        return Donation.objects.all()
 
     def perform_create(self, serializer):
         serializer.save()
+
+    def get_queryset(self):
+        # if self.request.user.is_superuser:
+        queryset = Donation.objects.all()
+        # else:
+        #     queryset = Donation.objects.filter(user=self.request.user)
+
+        return queryset
--- properties/views.py
+++ properties/views.py
@@ -6,17 +6,6 @@ from rest_framework.permissions import IsAuthenticated
 from .permissions import IsOwner
 # Create your views here
 
-# class PropertyViewSet(viewsets.ModelViewSet):
-#     serializer_class = PropertySerializer
-#     queryset = Property.objects.all()
-    
-
-#     def get_queryset(self, *args, **kwargs):
-#         return Property.objects.all()
-
-#     def perform_create(self, serializer):
-#         serializer.save()
-
 class PropertyViewSet(viewsets.ModelViewSet):
     serializer_class = PropertySerializer
     permission_classes = [IsOwner]
