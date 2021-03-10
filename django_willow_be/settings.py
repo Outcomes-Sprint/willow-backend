4bb80dc92b73b135a2586a5b79270087a86aa781
Victor Mantilla Colon <vmcolon@Victors-Air.home>
2021-03-10 15:01:39 -0500

    updating settings.py to include static

--- django_willow_be/settings.py
+++ django_willow_be/settings.py
@@ -147,6 +147,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         # What we'll use for our API
